1263783660ce3468c54e23a9a154ad952d2e9fb1
iopasd753951 <iopasd753951@gmail.com>
2020-06-19 15:07:42 +0900

    [추가] Token 기능 추가 에러발생

--- account/api/views.py
+++ account/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from account import models
 from . import serializers
@@ -12,3 +13,14 @@ class AccountViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserAccountSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnAccount,)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """유효한 필드 값인지 확인 후 Token 생성"""
+
+        return ObtainAuthToken().post(request)
+
+
