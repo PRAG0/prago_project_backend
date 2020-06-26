b4fd9a62f2eb3280d1f2d55d7998fd41bd734a6b
iopasd753951 <iopasd753951@gmail.com>
2020-06-26 15:09:55 +0900

    [추가] Search App생성

--- prago_manager/settings.py
+++ prago_manager/settings.py
@@ -39,12 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework.authtoken',
-
     'rest_framework',
 
     'taggit',
 
     'account',
+    'search',
 ]
 
 MIDDLEWARE = [
