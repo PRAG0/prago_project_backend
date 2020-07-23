0bd997673d775ed8464de10ceaa8f9e02f8514d0
iopasd753951 <iopasd753951@gmail.com>
2020-07-23 11:48:08 +0900

    [추가] tags 추가

--- search/api/views.py
+++ search/api/views.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
@@ -15,39 +15,17 @@ from bs4 import BeautifulSoup
 
 @method_decorator(csrf_exempt, name='dispatch')
 class SearchListView(View):
+    url = ['https://www.coupang.com/np/search?component=&q=', 'https://browse.gmarket.co.kr/search?keyword=']
+    prodt_name_tag = ['#\31 083851471 > a > dl > dd > div > div.name', 'div:nth-child(3) > div:nth-child(1) > div > div.box__information > div.box__information-major > div.box__item-title > span > a > span.text__item']
+    prodt_price_tag = ['', 'div:nth-child(3) > div:nth-child(1) > div > div.box__information > div.box__information-major > div.box__item-price > div > strong']
+    image_tag = ['', 'div:nth-child(3) > div:nth-child(1) > div > div.box__information > div.box__information-major > div.box__item-title > span > a > span.text__item']
+
     def post(self, request):
-        url_list = models.SiteInfo.objects.filter().values_list('url')
-        site_name_list = models.SiteInfo.objects.filter().values_list('site_name')
-        prodt_name_tag_list = models.SiteInfo.objects.filter().values_list('prodt_name_tag')
-        prodt_price_tag_list = models.SiteInfo.objects.filter().values_list('prodt_price_tag')
-        image_tag_list = models.SiteInfo.objects.filter().values_list('image_tag')
+        models.SiteInfo.objects.all().delete()
+        data = json.loads(request.body)
 
-        prodt_name = []
-        prodt_price = []
-        image = []
 
-        data = json.loads(str(request.body))
-
-        for i in range(len(models.SiteInfo.objects.count())):
-
-            req = requests.get(f'{url_list[i]}{data["query"]}')
-            html = req.text
-            soup = BeautifulSoup(html, 'html.parser')
-
-            prodt_name.append(soup.select(
-                prodt_name_tag_list[i]
-            )[0].text)
-
-            prodt_price.append(soup.select(
-                prodt_price_tag_list[i]
-            )[0].text)
-
-            # image.append(soup.select(
-            #     models.SiteInfo.image_tag[i]
-            # ))
-            print(prodt_price, prodt_name)
-
-        return
+        data = json.loads(request.body, encoding="UTF-8")
 
 
 
--- search/models.py
+++ search/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 
 class SiteInfo(models.Model):
-    url = models.URLField()
     site_name = models.CharField(max_length=255, null=True)
-    image_tag = models.CharField(max_length=255, null=True, blank=True)
-    prodt_price_tag = models.CharField(max_length=255, null=True, blank=True)
-    prodt_name_tag = models.CharField(max_length=255, null=True, blank=True)
+    prodt_name = models.CharField(max_length=255, null=True, blank=True)
+    prodt_price = models.CharField(max_length=255, null=True, blank=True)
+    image_url = models.CharField(max_length=255, null=True, blank=True)
