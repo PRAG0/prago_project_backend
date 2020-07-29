0325e93fd4730378aa3f947963a8d991e68adf1f
iopasd753951 <iopasd753951@gmail.com>
2020-07-30 00:16:12 +0900

    [추가] wishlistview 추가

--- search/api/views.py
+++ search/api/views.py
@@ -1,13 +1,16 @@
-import requests
-import json
-
-from search import models
+from account import models
+from search.models import SiteInfo
 from .crawl import crawlilng
+from . import serializers
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from account.api import permissions
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -18,7 +21,7 @@ class SearchListView(View):
         products_image = []
         products_site_name = []
         products_site_link = []
-        models.SiteInfo.objects.all().delete()
+        SiteInfo.objects.all().delete()
 
         query = request.GET.get('query')
         products_name, products_price, products_image, products_site_name, products_site_link = crawlilng(query)
@@ -32,7 +35,7 @@ class SearchListView(View):
             products_price[i] = match
 
         for i in range(len(products_name)):
-            models.SiteInfo(
+            SiteInfo(
                 index=i+1,
                 product_name=products_name[i],
                 product_price=products_price[i],
@@ -41,4 +44,12 @@ class SearchListView(View):
                 product_site_name=products_site_name[i],
             ).save()
 
-        return JsonResponse(list(models.SiteInfo.objects.values()), safe=False, status=200)
+        return JsonResponse(list(SiteInfo.objects.values()), safe=False, status=200)
+
+
+class WishListView(viewsets.ViewSet):
+    queryset = models.WishLists.objects.all()
+    serializer_class = serializers.WishListSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnAccount,)
+
