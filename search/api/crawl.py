98d2085650771ea72e29d6f6542408eb4a264cd9
iopasd753951 <iopasd753951@gmail.com>
2020-07-29 13:59:57 +0900

    [수정] 쇼핑몰이름이 리스트일 경우 전혀다른 단어가 파싱되는 경우 이미지 url 수정

--- search/api/crawl.py
+++ search/api/crawl.py
@@ -5,27 +5,29 @@ from bs4 import BeautifulSoup
 
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-
-products_name = []
-products_price = []
-products_image = []
-products_site_link = []
-products_site_name = []
-del_index = []
-
-options = Options()
-options.headless = True
-options.add_argument('disable-gpu')
-
-driver = webdriver.Chrome('c:\chromedriver_win32\chromedriver.exe', options=options)
-
-driver.implicitly_wait(10)
+from selenium.common.exceptions import InvalidSessionIdException
 
 
 def crawlilng(keyword):
+    prev_soup = ""
+    flag = 0
+    products_name = []
+    products_price = []
+    products_image = []
+    products_site_link = []
+    products_site_name = []
+    del_index = []
+
+    options = Options()
+    options.headless = True
+    options.add_argument('disable-gpu')
+
     for index in range(1, 2):
+        driver = webdriver.Chrome(executable_path=r'c:\chromedriver_win32\chromedriver.exe', options=options)
         driver.get(
             f'https://search.shopping.naver.com/search/all?frm=NVSHATC&origQuery={keyword}&pagingIndex={index}&pagingSize=40&productSet=total&query={keyword}&sort=rel&timestamp=&viewType=list')
+
+        time.sleep(3)
 
         SCROLL_PAUSE_TIME = 2
 
@@ -46,16 +48,18 @@ def crawlilng(keyword):
             last_height = new_height
 
         html = driver.page_source
-        driver.close()
+        driver.quit()
 
         soup = BeautifulSoup(html, 'html.parser')
+        # if prev_soup == soup:
+        #     return products_name, products_price, products_image, products_site_name, products_site_link
 
         ul = soup.find('ul', {'class': 'list_basis'})
 
         names = ul.findAll('div', {'class': 'basicList_title__3P9Q7'})
         prices = ul.findAll('strong', {'class': 'basicList_price__2r23_'})
         images = ul.findAll('div', {'class': 'thumbnail_thumb_wrap__1pEkS _wrapper'})
-        mall_names = ul.findAll('div', {'class': 'basicList_mall_title__3MWFY'})
+        mall_names = ul.findAll('div', {'class': 'basicList_mall_area__lIA7R'})
         lis = ul.findAll('div', {'class': 'thumbnail_thumb_wrap__1pEkS _wrapper'})
 
         for name in names[1:41]:
@@ -77,14 +81,18 @@ def crawlilng(keyword):
 
         for i in range(len(images)):
             text = images[i].find('a')['href']
-            image_id = text[-26: -15]
+            image_id = text[-26: -16]
             products_image.append(
-                f'https://shopping-phinf.pstatic.net/main_{image_id[:7]}/{image_id}.jpg?type=f140')
+                f'https://shopping-phinf.pstatic.net/main_{image_id[:8]}/{image_id}.jpg?type=f140')
 
+        for mall_name in mall_names[:41]:
+            mall_tag = mall_name.find('div', {'class': 'basicList_mall_title__3MWFY'})
+            mall = mall_tag.find('a')
+            if mall.text == "쇼핑몰별 최저가":
+                first_mall = mall_name.find('a', {'class': 'basicList_mall_item__tHbWj'})
+                products_site_name.append(first_mall['title'])
 
-        for mall_name in mall_names[1:41]:
-            mall = mall_name.find('a')
-            if mall.text:
+            elif mall.text:
                 products_site_name.append(mall.text)
             else:
                 buffer = mall.find('img')
@@ -94,4 +102,6 @@ def crawlilng(keyword):
             link = li.find('a')
             products_site_link.append(link['href'])
 
+        prev_soup = soup
+    print(products_image[0])
     return products_name, products_price, products_image, products_site_name, products_site_link
