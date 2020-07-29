601fe9087d6c61c3c67160323bb7972ba91c7ce2
iopasd753951 <iopasd753951@gmail.com>
2020-07-29 14:00:32 +0900

    [수정] 디비저장 방식 및 return json형식 수정

--- search/api/views.py
+++ search/api/views.py
@@ -4,34 +4,41 @@ import json
 from search import models
 from .crawl import crawlilng
 
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
-from rest_framework import permissions
-
-from bs4 import BeautifulSoup
-
 
 @method_decorator(csrf_exempt, name='dispatch')
 class SearchListView(View):
-    def post(self, request):
-
+    def get(self, request):
+        products_name = []
+        products_price = []
+        products_image = []
+        products_site_name = []
+        products_site_link = []
         models.SiteInfo.objects.all().delete()
 
-        data = json.loads(request.body)
-        products_name, products_price, products_image, products_site_name, products_site_link = crawlilng(data['query'])
+        query = request.GET.get('query')
+        products_name, products_price, products_image, products_site_name, products_site_link = crawlilng(query)
 
-        models.SiteInfo(
-            product_name=products_name,
-            product_price=products_price,
-            product_image=products_image,
-            product_site_link=products_site_link,
-            product_site_name=products_name,
-        ).save()
+        for i in range(len(products_price)):
+            match = products_price[i] \
+                .replace('최저', '') \
+                .replace('원', '') \
+                .replace(',', '')
 
-        return HttpResponse(status=200)
+            products_price[i] = match
 
-    def get(self, request):
-        return JsonResponse({'search list': list(models.SiteInfo.objects.values())}, status=200)
+        for i in range(len(products_name)):
+            models.SiteInfo(
+                index=i+1,
+                product_name=products_name[i],
+                product_price=products_price[i],
+                product_image=products_image[i],
+                product_site_link=products_site_link[i],
+                product_site_name=products_site_name[i],
+            ).save()
+
+        return JsonResponse(list(models.SiteInfo.objects.values()), safe=False ,status=200)
