365311339b6fcd899831c1d2fb0efee8753aa323
iopasd753951 <iopasd753951@gmail.com>
2020-07-29 14:01:21 +0900

    [수정] id필드추가 및 product_image필드 intfield로 수정

--- search/models.py
+++ search/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class SiteInfo(models.Model):
-    product_name = models.CharField(max_length=255, default="")
-    product_price = models.CharField(max_length=255, default="")
+    index = models.AutoField(primary_key=True)
+    product_name = models.TextField(max_length=255, default="")
+    product_price = models.IntegerField()
     product_image = models.TextField(default="")
-    product_site_name = models.CharField(max_length=255, default="")
+    product_site_name = models.TextField(max_length=255, default="")
     product_site_link = models.TextField(default="")
 
     class Meta:
-        ordering = ['product_price'*1]
+        ordering = ['product_price']
