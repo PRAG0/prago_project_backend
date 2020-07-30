2299767c4dae47a4abc193d02eb0b111a21b7101
iopasd753951 <iopasd753951@gmail.com>
2020-07-30 15:02:15 +0900

    [수정] wishlist기능 분리로 인한 삭제조치

--- account/models.py
+++ account/models.py
@@ -99,18 +99,3 @@ class UserAccounts(AbstractBaseUser, PermissionsMixin):
     class Meta:
         ordering = ['user_id']
         verbose_name = "useraccount"
-
-
-class WishLists(models.Model):
-    """찜 목록 모델"""
-
-    user_id = models.ForeignKey(UserAccounts, on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=255)
-    product_price = models.CharField(max_length=255)
-    product_image = models.TextField(default="")
-    product_site_link = models.TextField()
-    product_site_name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['-created_at']
