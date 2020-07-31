1c4b6a6abdd2e40da5290ea2262469cc218221c7
iopasd753951 <iopasd753951@gmail.com>
2020-07-31 18:59:43 +0900

    [수정] viewset create함수 삭제

--- wishlist/api/views.py
+++ wishlist/api/views.py
@@ -1,11 +1,15 @@
 from wishlist import models
 from . import serializers
 
+from django.http import JsonResponse, HttpResponse
 from rest_framework import viewsets
-from account.api import permissions
 
 
 class WishListViewSet(viewsets.ModelViewSet):
     queryset = models.WishLists.objects.all()
     serializer_class = serializers.WishListSerializer
     # permission_classes = (permissions.UpdateOwnAccount,)
+
+    # def create(self, request, *args, **kwargs):
+    #
+    #     return HttpResponse(self.queryset.all())
